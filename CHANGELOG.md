a8fee60d0f4d1b90578964440e630ceefb689f3c
yshrsmz <the.phantom.bane@gmail.com>
2015-07-03 19:13:55 +0900

    v0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+Change Log
+===
+
+Version 0.1.0 *(2015-7-3)*
+--------------------------
+
+- Rewrite `background.js` from scratch.
+- NEW: Share current page by type `tw :share`
+
+
+Version 0.0.2 *(2015-6-27)*
+---------------------------
+
+- Initial release.
+
--- src/js/background.js
+++ src/js/background.js
@@ -1,38 +1,107 @@
 'use strict';
 
-let oauth = ChromeExOAuth.initBackgroundPage({
-    'request_url': 'https://api.twitter.com/oauth/request_token',
-    'authorize_url': 'https://api.twitter.com/oauth/authorize',
-    'access_url': 'https://api.twitter.com/oauth/access_token',
-    'consumer_key': TwitterAPIKey.consumer_key,
-    'consumer_secret': TwitterAPIKey.consumer_secret,
-    'scope': 'https://api.twitter.com/1.1/statuses/update.json',
-    'app_name': 'Omnitweety'
-});
+class OmniTweety {
 
-let notify = (iconUrl, title, message) => {
-    chrome.notifications.create('omnitweety', {
-        type: 'basic',
-        iconUrl: iconUrl,
-        title: title,
-        message: message
-    }, (id) => {
-        window.setTimeout(() => {
-            chrome.notifications.clear(id, function () {
-            });
-        }, 3000);
-    });
-};
+    static get URL_UPDATE() {
+        return 'https://api.twitter.com/1.1/statuses/update.json';
+    }
 
-oauth.authorize(() => {
-    chrome.omnibox.onInputChanged.addListener((text) => {
-        chrome.omnibox.setDefaultSuggestion({
-            description: `${140 - text.length} characters remaining.`
+    static get URL_CONFIG() {
+        return 'https://api.twitter.com/1.1/help/configuration.json';
+    }
+
+    static get shareCommand() {
+        return /^:share\s?/;
+    }
+
+    constructor() {
+        this.oauth = this.initBackgroundPage();
+
+        this.oauth.authorize(this.handleEvents.bind(this));
+    }
+
+    initBackgroundPage() {
+        return ChromeExOAuth.initBackgroundPage({
+            'request_url': 'https://api.twitter.com/oauth/request_token',
+            'authorize_url': 'https://api.twitter.com/oauth/authorize',
+            'access_url': 'https://api.twitter.com/oauth/access_token',
+            'consumer_key': TwitterAPIKey.consumer_key,
+            'consumer_secret': TwitterAPIKey.consumer_secret,
+            'scope': 'https://api.twitter.com/1.1/statuses/update.json,https://api.twitter.com/1.1/help/configuration.json',
+            'app_name': 'Omnitweety'
         });
-    });
+    }
 
-    chrome.omnibox.onInputEntered.addListener((text) => {
-        let url = 'https://api.twitter.com/1.1/statuses/update.json';
+    notify(iconUrl, title, message) {
+        chrome.notifications.create('omnitweety', {
+            type: 'basic',
+            iconUrl: iconUrl,
+            title: title,
+            message: message
+        }, (id) => {
+            window.setTimeout(() => {
+                chrome.notifications.clear(id, () => {
+                });
+            }, 3000);
+        });
+    }
+
+    isShareCommand(text) {
+        return OmniTweety.shareCommand.test(text);
+    }
+
+    getCurrentPage(cb) {
+        chrome.tabs.query({active: true, currentWindow: true}, (pages) => {
+            if (pages.length > 0) {
+                cb(pages[0]);
+            } else {
+                cb(null);
+            }
+        });
+    }
+
+    handleEvents() {
+        chrome.omnibox.onInputChanged.addListener((text) => {
+            if (this.isShareCommand(text)) {
+
+                this.getCurrentPage((page) => {
+                    let message = 'unable to share this page';
+                    this.getShortUrlMaxLength((urlMaxLength = page.url.length) => {
+                        if (page) {
+                            message = this.buildShareText('NowBrowsing', page.title, page.url, urlMaxLength);
+                        }
+
+                        chrome.omnibox.setDefaultSuggestion({
+                            description: message
+                        });
+                    });
+                });
+            } else {
+                chrome.omnibox.setDefaultSuggestion({
+                    description: `${140 - text.length} characters remaining.`
+                });
+            }
+        });
+
+        chrome.omnibox.onInputEntered.addListener((text) => {
+            if (this.isShareCommand(text)) {
+
+                this.getCurrentPage((page) => {
+                    this.getShortUrlMaxLength((maxLength = page.url.length) => {
+                        let sendMessage = this.buildShareText('NowBrowsing', page.title, page.url, maxLength);
+
+                        this.postStatus(sendMessage);
+                    });
+
+                });
+            } else {
+                this.postStatus(text);
+            }
+        });
+    }
+
+    postStatus(text) {
+
         let request = {
             method: 'POST',
             parameters: {
@@ -40,17 +109,60 @@ oauth.authorize(() => {
             }
         };
 
-        oauth.sendSignedRequest(
-            url,
-            (response, xhr) => {
-                let result = JSON.parse(response);
-                if (result.errors !== undefined) {
-                    notify('./assets/icon_128.png', 'Oops! There was an error.', result.errors[0].message);
-                } else {
-                    notify(result.user.profile_image_url_https, result.user.name,
-                        result.text);
-                }
-            },
-            request);
-    })
-});
+        this.oauth.sendSignedRequest(OmniTweety.URL_UPDATE, (response, xhr) => {
+            let result = JSON.parse(response);
+
+            if (result.errors != undefined) {
+                this.notify('./assets/icon_128.png', 'Oops! There was an error.', result.errors[0].message);
+            } else {
+                this.notify(result.user.profile_image_url_https, result.user.name, result.text);
+            }
+        }, request);
+    }
+
+    getTwitterConfig(cb) {
+        let request = {
+            method: 'POST'
+        };
+
+        let lastUpdatedAt = localStorage['twitter_config_last_updated_at'];
+        let config = localStorage['twitter_config'];
+
+        lastUpdatedAt = lastUpdatedAt ? +lastUpdatedAt : 0;
+
+        if (new Date().getTime() - lastUpdatedAt > 86400000 || !config) {
+            this.oauth.sendSignedRequest(OmniTweety.URL_CONFIG, (response, xhr) => {
+                localStorage['twitter_config'] = response;
+                localStorage['twitter_config_last_updated_at'] = new Date().getTime();
+
+                cb(JSON.parse(response));
+            });
+        } else {
+            cb(JSON.parse(config));
+        }
+    }
+
+    getShortUrlMaxLength(cb) {
+        this.getTwitterConfig((config) => {
+            if (!config) {
+                cb(null);
+            } else {
+                cb(config.short_url_length_https);
+            }
+        });
+    }
+
+    buildShareText(prefix, title, url, urlMaxLength) {
+        let baseMessage = `${prefix}: ${title} `;
+        let lengthDiff = 140 - (baseMessage.length + urlMaxLength);
+
+        if (lengthDiff < 0) {
+            title = title.slice(0, lengthDiff);
+        }
+
+        return `${prefix}: ${title} ${url}`;
+    }
+
+}
+
+new OmniTweety();
