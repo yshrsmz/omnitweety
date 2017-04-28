6589d5737b25da1e98f793ca9a7db63f2edb5646
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2017-04-28 21:08:56 +0900

    :construction: more sub commands

--- v1/src/ts/background/sagas/parse_status.ts
+++ v1/src/ts/background/sagas/parse_status.ts
@@ -1,11 +1,13 @@
 import minimist, { IArgv, IOptions } from "minimist";
 
 const options: IOptions = {
-    boolean: ["t", "s", "l"],
+    boolean: ["t", "s", "l", "v", "o"],
     alias: {
         t: "twitter",
         s: "share",
         l: "slack",
+        v: "version",
+        o: "options",
     },
     stopEarly: true,
 };
@@ -14,22 +16,35 @@ interface IArgs extends IArgv {
     share: boolean;
     twitter: boolean;
     slack: boolean;
+    version: boolean;
+    options: boolean;
 }
 
 export interface IParsedStatus {
     share: boolean;
     slack: boolean;
     twitter: boolean;
+    version: boolean;
+    options: boolean;
     status: string;
 }
 
 export default function parseStatus(value: string): IParsedStatus {
     const args: IArgs = minimist<IArgs>(value.split(" "), options);
 
+    /**
+     * TODO: handle old sub commands
+     *
+     * - [ ] :share
+     * - [ ] :options
+     * - [ ] :version
+     */
     return {
         share: args.share,
         slack: args.slack,
         twitter: args.twitter,
+        version: args.version,
+        options: args.options,
         status: args._.join(" "),
     };
 }
