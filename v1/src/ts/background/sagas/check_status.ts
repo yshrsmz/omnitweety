e71acaa2784116fa5433c50b997e3669608ac504
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2017-05-08 20:18:52 +0900

    get web page info

--- v1/src/ts/background/sagas/check_status.ts
+++ v1/src/ts/background/sagas/check_status.ts
@@ -4,18 +4,57 @@ import { call, cancel, cancelled, fork, put, race, take, takeEvery } from "redux
 import * as Actions from "../actions";
 import parseStatus from "../../parser/status_parser";
 
-function* runCalculateStatus({ payload }: { type: string, payload: string }) {
+interface IPage {
+    title: string;
+    url: string;
+}
+
+async function getWebPageInfo() {
+    return new Promise<IPage>((resolve, reject) => {
+        chrome.tabs.query({active: true, currentWindow: true}, (pages) => {
+            if (pages.length > 0) {
+                resolve({ title: pages[0].title, url: pages[0].url} );
+            } else {
+                reject(null);
+            }
+        });
+    });
+}
+
+function* runCalculateStatus(fixed: boolean, { payload }: { type: string, payload: string }) {
     const parsed = parseStatus(payload);
-    console.log(parsed);
-    yield put(Actions.notifyStatusPartsUpdated(null));
+
+    let page: IPage = null;
+    if (parsed.share) {
+        page = yield getWebPageInfo()
+    }
+
+    const newPayload: Actions.IStatusPartsPayload = {
+        flags: {
+            options: parsed.options,
+            share: parsed.share,
+            slack: parsed.slack,
+            twitter: parsed.twitter,
+            version: parsed.version,
+        },
+        web: {
+            url: parsed.share ? page.url : "",
+            title: parsed.share ? page.title : "",
+        },
+        userInput: parsed.status,
+        content: "",
+        fixed: fixed,
+    };
+    console.log("newPayload", newPayload);
+    yield put(Actions.notifyStatusPartsUpdated(newPayload));
 }
 
 function* handleStatusUpdated() {
-    yield takeEvery(Actions.UPDATE_OMNIBOX, runCalculateStatus);
+    yield takeEvery(Actions.UPDATE_OMNIBOX, runCalculateStatus, false);
 }
 
-function handleStatusFixed() {
-
+function* handleStatusFixed() {
+    yield takeEvery(Actions.FIX_OMNIBOX, runCalculateStatus, true);
 }
 
 function* runCancelStatus() {
