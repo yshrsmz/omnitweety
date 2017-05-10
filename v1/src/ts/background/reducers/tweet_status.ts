0108ff812edb1cef1ba9ff930f90440b4d459551
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2017-05-10 19:46:47 +0900

    :construction: implement tweet reducer

--- v1/src/ts/background/reducers/tweet_status.ts
+++ v1/src/ts/background/reducers/tweet_status.ts
@@ -1,3 +1,4 @@
+import { combineReducers } from "redux";
 import * as Actions from "../actions";
 import { IPayloadAction, IStatusPartsPayload } from "../actions";
 
@@ -33,10 +34,11 @@ const initialStateusContent = {
     fixed: false,
 };
 
-const tweetStatusFlags = (state: IStatusFlags = initialStatusFlags, action: IPayloadAction<IStatusPartsPayload>) => {
-    const flags = action.payload.flags;
+const statusFlags = (state: IStatusFlags = initialStatusFlags, action: IPayloadAction<IStatusPartsPayload>) => {
+    let flags;
     switch (action.type) {
         case Actions.STATUS_PARTS_UPDATED:
+            flags = action.payload.flags;
             return {
                 ...state,
                 options: flags.options,
@@ -46,6 +48,7 @@ const tweetStatusFlags = (state: IStatusFlags = initialStatusFlags, action: IPay
                 version: flags.version,
             };
         case Actions.STATUS_RESET:
+            flags = action.payload.flags;
             return {
                 ...state,
                 options: false,
@@ -59,15 +62,24 @@ const tweetStatusFlags = (state: IStatusFlags = initialStatusFlags, action: IPay
     }
 };
 
-const tweetStatusContent = (state: IStatusContent = initialStateusContent, action: IPayloadAction<IStatusPartsPayload>) => {
+const statusContent = (state: IStatusContent = initialStateusContent, action: IPayloadAction<IStatusPartsPayload>) => {
     switch (action.type) {
         case Actions.STATUS_PARTS_UPDATED:
+            const payload = action.payload;
             return {
                 ...state,
+                userInput: payload.userInput,
+                composed: payload.content,
+                length: payload.length,
+                fixed: payload.fixed,
             };
         case Actions.STATUS_RESET:
             return {
                 ...state,
+                userInput: "",
+                composed: "",
+                length: 0,
+                fixed: false,
             };
         default:
             return state;
@@ -76,3 +88,10 @@ const tweetStatusContent = (state: IStatusContent = initialStateusContent, actio
 
 export const getTweetStatusFlags = (state: any): IStatusFlags => state.tweetStatusFlags;
 export const getTweetStatusContent = (state: any): IStatusContent => state.tweetStatusContent;
+
+const tweet = combineReducers({
+    statusFlags,
+    statusContent,
+});
+
+export default tweet;
