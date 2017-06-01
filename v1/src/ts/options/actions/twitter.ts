2803193b9bc08135888bf88ad61ffccdb7e545a0
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2017-06-01 13:34:43 +0900

    :construction: login actions

--- v1/src/ts/options/actions/twitter.ts
+++ v1/src/ts/options/actions/twitter.ts
@@ -1,1 +1,20 @@
 import { IAction, IPayloadAction } from "../../common/actions";
+import AccessToken from "../../data/access_token";
+
+export const LOGIN_REQUESTED = "LOGIN_REQUESTED";
+export const notifyLoginRequested = (): IAction => ({ type: LOGIN_REQUESTED });
+
+export const LOGIN_PINCODE_RECEIVED = "LOGIN_PINCODE_RECEIVED";
+export const notifyLoginPincodeRecceived = (pinCode: string): IPayloadAction<string> => ({
+    type: LOGIN_PINCODE_RECEIVED,
+    payload: pinCode,
+});
+
+export const LOGIN_TOKEN_RECEIVED = "LOGIN_TOKEN_RECEIVED";
+export const notifyLoginTokenReceived = (token: string, tokenSecret: string): IPayloadAction<AccessToken> => ({
+    type: LOGIN_TOKEN_RECEIVED,
+    payload: new AccessToken(token, tokenSecret),
+});
+
+export const LOGIN_USER_RECEIVED = "LOGIN_USER_RECEIVED";
+export const notifyLoginUserReceived = ():IAction => ({type: LOGIN_USER_RECEIVED});
