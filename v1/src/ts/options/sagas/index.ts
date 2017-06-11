1bba33de0e67e592690377cb27fcf4ec70ca7fe6
yshrsmz <the.phantom.bane+github@gmail.com>
2017-06-11 22:45:30 +0900

    :construction: login

--- v1/src/ts/options/sagas/index.ts
+++ v1/src/ts/options/sagas/index.ts
@@ -2,8 +2,10 @@ import { fork } from "redux-saga/effects";
 
 import * as prefix from "./prefix";
 import * as slack from "./slack";
+import * as login from "./login";
 
 export default function* rootSaga() {
+    yield fork(login.handleLoginEvents);
     yield fork(prefix.handlePrefixConfigEvents);
     yield fork(slack.handleSlackConfigEvents);
 }
--- v1/src/ts/options/sagas/login.ts
+++ v1/src/ts/options/sagas/login.ts
@@ -2,7 +2,7 @@ import { call, fork, put, takeEvery } from "redux-saga/effects";
 
 import { IPayloadAction } from "../../common/actions";
 import configDataStore from "../../data/config_datastore";
-import * as Actions from "../actions/twitter";
+import * as Actions from "../actions/login";
 import accessTokenRepository from "../../data/access_token_repository";
 import authorizer from "../../data/authorizer";
 
@@ -15,4 +15,8 @@ function runLoginRequested() {
 
 function* handleLoginRequested() {
     yield takeEvery(Actions.LOGIN_REQUESTED, runLoginRequested);
+}
+
+export function* handleLoginEvents() {
+    yield fork(handleLoginRequested)
 }
