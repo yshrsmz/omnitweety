fb0f1c1d9d8c0e9a60f0dd8972b1095dfd0beabe
yshrsmz <the.phantom.bane@gmail.com>
2020-01-13 23:39:09 +0900

    :constructIon: setting ui

---
+++ src/ts/options/components/AmazonAssociateListItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-list-item>
+      <v-select :items="amazonDomains" label="Your Amazon Domain" clearable @input="onDomainChanged" :v-model="currentDomain"/>
+    </v-list-item>
+    <v-list-item />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import { Getter } from "vuex-class";
+
+@Component({
+  name: "amazon-associate-list-item"
+})
+export default class AmazonAssociateListItem extends Vue {
+
+  @Getter('amazonDomains') amazonDomains: string[]
+
+  currentDomain: string = ''
+
+  onDomainChanged(value: string) {
+    console.log('domainChanged:', value)
+  }
+}
+</script>
--- src/ts/options/components/SettingList.vue
+++ src/ts/options/components/SettingList.vue
@@ -7,6 +7,9 @@
       <v-subheader>General</v-subheader>
       <tweet-template-list-item />
       <v-divider />
+      <v-subheader>Amazon Associate</v-subheader>
+      <amazon-associate-list-item />
+      <v-divider />
       <v-subheader>Others</v-subheader>
       <v-list-item>
         <v-list-item-content>
@@ -14,19 +17,13 @@
           <v-list-item-subtitle>{{ appVersion }}</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
-      <v-list-item
-        :href="developerUrl"
-        target="_blank"
-      >
+      <v-list-item :href="developerUrl" target="_blank">
         <v-list-item-content>
           <v-list-item-title>Developer</v-list-item-title>
           <v-list-item-subtitle>{{ developerName }}</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
-      <v-list-item
-        :href="webstoreUrl"
-        target="_blank"
-      >
+      <v-list-item :href="webstoreUrl" target="_blank">
         <v-list-item-content>
           <v-list-item-title>Rate on Chrome Webstore</v-list-item-title>
         </v-list-item-content>
@@ -36,29 +33,30 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import { mapState, mapGetters } from 'vuex';
-import { Component, Prop } from 'vue-property-decorator';
-import AuthListItem from './AuthListItem';
-import TweetTemplateListItem from './TweetTemplateListItem';
-import { AppConfig } from '../../Config';
+import Vue from "vue";
+import { mapState, mapGetters } from "vuex";
+import { Component, Prop } from "vue-property-decorator";
+import AuthListItem from "./AuthListItem";
+import TweetTemplateListItem from "./TweetTemplateListItem";
+import AmazonAssociateListItem from "./AmazonAssociateListItem";
+import { AppConfig } from "../../Config";
 
 @Component({
-  name: 'setting-list',
+  name: "setting-list",
   computed: {
-    ...mapGetters(['tweetTemplate'])
+    ...mapGetters(["tweetTemplate"])
   },
   components: {
     AuthListItem,
-    TweetTemplateListItem
+    TweetTemplateListItem,
+    AmazonAssociateListItem
   }
 })
 export default class SettingList extends Vue {
-
   isPrefixDialogActive = false;
 
   developerName: string = AppConfig.DEVELOPER_NAME;
-  developerUrl: string = AppConfig.URL_DEVELOPER
+  developerUrl: string = AppConfig.URL_DEVELOPER;
 
   webstoreUrl: string = AppConfig.URL_CHROME_WEBSTORE;
 
