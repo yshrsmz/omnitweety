bde00bc31f6dac034332a5991e4c406829a06d17
yshrsmz <the.phantom.bane@gmail.com>
2020-01-15 21:15:26 +0900

    finish setting ui

--- src/ts/options/components/AmazonAssociateListItem.vue
+++ src/ts/options/components/AmazonAssociateListItem.vue
@@ -1,28 +1,112 @@
 <template>
   <div>
     <v-list-item>
-      <v-select :items="amazonDomains" label="Your Amazon Domain" clearable @input="onDomainChanged" :v-model="currentDomain"/>
+      <v-list-item-content>
+        <v-select
+          v-model="currentDomain"
+          :items="amazonDomains"
+          label="Your Amazon Domain"
+          clearable
+          value="currentDomain"
+          @input="onDomainChanged"
+        />
+      </v-list-item-content>
     </v-list-item>
-    <v-list-item />
+    <v-list-item @click.stop="onAssociateIdClick">
+      <v-list-item-content>
+        <v-list-item-title>Amazon Associate ID</v-list-item-title>
+        <v-list-item-subtitle>{{ associateIdOrEmpty }}</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+    <v-dialog
+      v-model="isAssociateIdDialogActive"
+      max-width="500px"
+    >
+      <v-card>
+        <v-card-title>Edit Associate ID</v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="associateId"
+            label="Enter New Associate ID"
+          />
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            text
+            @click.stop="isAssociateIdDialogActive=false"
+          >
+            Close
+          </v-btn>
+          <v-btn
+            color="primary"
+            text
+            @click.stop="onUpdateAssociateIdRequested"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
-import { Getter } from "vuex-class";
+import { Action, Getter } from "vuex-class";
+import AmazonAssociate from "../../data/AmazonAssociate";
 
 @Component({
   name: "amazon-associate-list-item"
 })
 export default class AmazonAssociateListItem extends Vue {
+  @Getter("amazonDomains") amazonDomains: string[];
 
-  @Getter('amazonDomains') amazonDomains: string[]
+  @Getter("amazonAssociate") amazonAssociate: AmazonAssociate;
 
-  currentDomain: string = ''
+  @Action("updateAmazonAssociate") updateAmazonAssociate;
+
+  isAssociateIdDialogActive = false;
+
+  currentDomain = "";
+  associateId = "";
+
+  get associateIdOrEmpty(): string {
+    if (this.amazonAssociate.associateId === "") {
+      return "-";
+    } else {
+      return this.amazonAssociate.associateId;
+    }
+  }
+
+  mounted() {
+    this.currentDomain = this.amazonAssociate.domain
+    this.associateId = this.amazonAssociate.associateId;
+  }
 
   onDomainChanged(value: string) {
-    console.log('domainChanged:', value)
+    this.doUpdateAmazonAssociate();
+  }
+
+  onAssociateIdClick() {
+    this.associateId = this.amazonAssociate.associateId;
+    this.isAssociateIdDialogActive = true;
+  }
+
+  onUpdateAssociateIdRequested() {
+    this.doUpdateAmazonAssociate();
+    this.isAssociateIdDialogActive = false;
+  }
+
+  doUpdateAmazonAssociate() {
+    console.log('doUpdateAmazonAssociate')
+    const newAmazonAssociate = new AmazonAssociate(
+      this.currentDomain || "",
+      this.associateId || ""
+    );
+    this.updateAmazonAssociate(newAmazonAssociate);
   }
 }
 </script>
