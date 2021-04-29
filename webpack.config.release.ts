485f9b51f14a03a45ac72220071d370f09b1a8ae
yshrsmz <the.phantom.bane@gmail.com>
2021-04-29 20:37:07 +0900

    :arrow_up: webpack & vue

--- webpack.config.release.ts
+++ webpack.config.release.ts
@@ -1,5 +1,5 @@
 import webpack from 'webpack'
-import merge from 'webpack-merge'
+import { merge } from 'webpack-merge'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 import baseConfig from './webpack.config.base'
